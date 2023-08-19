9cf5a46ac26b739ae2684325f862953a40faf9cb
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-08-19 15:19:52 -0500

    Add files via upload

---
+++ tricks.py
@@ -0,0 +1,40 @@
+from flask import render_template, redirect, request
+from flask_app import app
+from flask_app.models.skaters import Dojo
+from flask_app.models.tricks import Ninja
+
+@app.route('/')
+def index():
+    return render_template("dojos_main.html")
+
+@app.route('/ninjas')
+def ninjas():
+    ninjas = Ninja.get_all()
+    return render_template('ninja.html', ninjas=ninjas)
+
+@app.route('/new/ninja')
+def new_ninja():
+    dojos = Dojo.get_all()
+    return render_template('new_ninja.html', dojos=dojos)
+
+@app.route('/create/ninja', methods=['POST'])
+def create_ninja():
+    Ninja.save(request.form)
+    return redirect('/')
+
+
+
+# @app.route('/ninjas/edit/<ninja_id>')
+# def edit_page(ninja_id):
+#     print("Editing ninja id:", ninja_id)
+#     return render_template('edit_ninja.html', ninja=ninja)
+
+# @app.route('/ninjas/delete/<ninja_id>/<dojo_id>')
+# def delete_ninja(ninja_id, dojo_id):
+#     print("Deleting ninja id:", ninja_id)
+#     return redirect(f'/dojos/{dojo_id}')
+
+# @app.route('/ninjas/update/<dojo_id>', methods=["POST"])
+# def update_ninja(dojo_id):
+#     print("Updating ninja information: ", request.form)
+#     return redirect(f'/dojos/{dojo_id}')
