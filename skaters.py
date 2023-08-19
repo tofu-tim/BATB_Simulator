56a7c084be454f1f0fc5569c429e7beb4294940c
Timothy Wilger <47797775+tofu-tim@users.noreply.github.com>
2023-08-19 15:20:08 -0500

    Add files via upload

--- skaters.py
+++ skaters.py
@@ -1,14 +1,42 @@
-from flask import Flask, render_template, redirect, request
-from flask_app import app
-from flask_app.models.skaters import Dojo
-from flask_app.models.tricks import Ninja
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models.tricks import Tricks
 
-@app.route('/dojos')
-def dojos():
-    dojos = Dojo.get_all()
-    return render_template('dojos_main.html', dojos=dojos)
+class Skater:
+    db = 'skaters_and_tricks'
+    def __init__(self, data):
+        self.id = data['id']
+        self.name = data['name']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']    
+        self.tricks = []
 
-@app.route('/add/dojo', methods=['POST'])
-def add_dojo():
-    Dojo.save(request.form)
-    return redirect('/')
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM skaters;"
+        results = connectToMySQL(cls.db).query_db(query)
+        skaters = []
+        for skater in results:
+            skater.append( cls(skater) )
+        return skaters
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO skaters (name) VALUES (%(name)s);"
+        result = connectToMySQL(cls.db).query_db(query,data)
+        return result
+
+    @classmethod
+    def get_one(cls,data):
+        query  = "SELECT * FROM skaters WHERE id = %(id)s;"
+        result = connectToMySQL(cls.db).query_db(query,data)
+        return cls(result[0])
+
+    @classmethod
+    def update(cls,data):
+        query = "UPDATE dojos SET name=%(name)s,updated_at=NOW() WHERE id = %(id)s;"
+        return connectToMySQL(db).query_db(query,data)
+
+    @classmethod
+    def destroy(cls,data):
+        query  = "DELETE FROM dojos WHERE id = %(id)s;"
+        return connectToMySQL(db).query_db(query,data)
